bc33c92844b55cd170be788427fa9ee24b204c22
doraemonzero <leo1202@protonmail.com>
2022-03-04 17:43:42 +0800

    modified univ3 swapper to add setters for the pool fees

--- contracts/utils/Univ3Swapper.sol
+++ contracts/utils/Univ3Swapper.sol
@@ -12,13 +12,15 @@ contract Univ3Swapper {
     address public swapRouter;
     address public weth;
     uint24 public poolFee1;
-    uint24 public poolFee2 ;
+    uint24 public poolFee2;
+    address public governor;
 
     constructor(address _swapRouter, address _weth, uint24 _poolFee1, uint24 _poolFee2) {
         swapRouter = _swapRouter;
         weth = _weth;
         poolFee1 = _poolFee1;
         poolFee2 = _poolFee2;
+        governor = msg.sender;
     }
 
     function swap(address tokenIn, address tokenOut, uint256 amountIn) external {
@@ -34,6 +36,20 @@ contract Univ3Swapper {
         amountOutMinimum: 0
         });
         ISwapRouter(swapRouter).exactInput(params);
+    }
+
+    function setPoolFees(uint24 _poolFee1, uint24 _poolFee2) external onlyGovernor {
+        poolFee1 = _poolFee1;
+        poolFee2 = _poolFee2;
+    }
+
+    function setGovernor(address _governor) external onlyGovernor {
+        governor = _governor;
+    }
+
+    modifier onlyGovernor() {
+        require(msg.sender == governor, "!governor");
+        _;
     }
 
 }
